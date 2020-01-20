5ff89eed4c313c15185bf3729aeb71de058b13d1
hayobabson <babalolakehinde.p@gmail.com>
2020-01-20 18:46:51 +0100

    project setup

---
+++ src/test/java/com/kennybabs/pollarserver/PollarserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennybabs.pollarserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollarserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
